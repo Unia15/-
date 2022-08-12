253519396df3ba7b4a9977d36d49fcd8e0d0e7ff
Julia.Ef <jullia.ef@yandex.ru>
2022-08-12 13:13:16 +0300

    Primer2

---
+++ Primer2/Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+int b;
+
+Console.WriteLine("Введите два числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+   Console.WriteLine("{0} наибольшее число", a);
+}
+else
+{
+   Console.WriteLine("{0} наибольшее число", b);
+}
