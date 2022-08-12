72bfc8f5a759b998ba39c2bf5222d27e8c8544a9
Julia.Ef <jullia.ef@yandex.ru>
2022-08-12 13:19:14 +0300

    Primer4

---
+++ Primer4/Program.cs
@@ -0,0 +1,38 @@
+﻿int a;
+int b;
+int c;
+
+Console.WriteLine("Введите три числа");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+
+if(a > b)
+{
+    if (a > c)
+    {
+        Console.WriteLine("{0} самое большое число", a);
+    }
+    else if (c > b)
+    {
+        Console.WriteLine("{0} самое большое число", c);
+    }
+    else
+    {
+        Console.WriteLine("{0} самое большое число", b);
+    }
+   
+}
+else if (b > a)
+{
+    if (b > c)
+    {
+        Console.WriteLine("{0} самое большое число", b);
+    }
+    else 
+    {
+        Console.WriteLine("{0} самое большое число", c);
+    }
+   
+}
