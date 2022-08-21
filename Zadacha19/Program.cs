ddd193244f45c8d3bce1e3a0081431d4ad76cc95
Julia.Ef <jullia.ef@yandex.ru>
2022-08-21 14:51:08 +0300

    Zadacha19

---
+++ Zadacha19/Program.cs
@@ -0,0 +1,19 @@
+﻿int a;
+
+Console.WriteLine("Введите число");
+
+a = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(a);
+
+if (number[0]==number[4] || number[1]==number[3])
+{
+    Console.WriteLine($"{a} - палиндром");  
+}
+else
+{
+    Console.WriteLine($"{a} - не палиндром");
+} 
+
+
+
+
