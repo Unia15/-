88318cae2baf004eb162e5f8b87fb9a2787c354c
Julia.Ef <jullia.ef@yandex.ru>
2022-09-02 17:54:31 +0300

    Zadacha64

---
+++ Zadacha64/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+//M = 4; N = 8. -> ""4, 6, 7, 8""
+
+int M;
+int N;
+
+Console.Write("Введи M: ");
+ M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+ N = Convert.ToInt16(Console.ReadLine());
+
+
+if (N>M)
+    for (int i = M; i <= N; i++)
+    {
+    Console.Write($" {i}");
+    }
+    
+else
+    for (int i = N; i <= M; i++)
+    {
+      Console.Write($" {i}");
+    }
+
+    
+        
